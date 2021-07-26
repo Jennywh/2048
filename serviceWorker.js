c6cf52ed3ede8a43d6c1369033c507a834438cf5
Han Wang <jennyhan_wang@outlook.com>
2021-07-26 23:47:46 +0800

    4096

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,33 +1,31 @@
 // This is the service worker with the Cache-first network
 
-const CACHE = "pwabuilder-precache";
-const precacheFiles = [
-  'index.html', 'index.js'
-];
+const CACHE = 'pwabuilder-precache'
+const precacheFiles = ['index.html', 'index.js']
 
-self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
+self.addEventListener('install', function (event) {
+  console.log('[PWA Builder] Install Event processing')
 
-  console.log("[PWA Builder] Skip waiting on install");
-  self.skipWaiting();
+  console.log('[PWA Builder] Skip waiting on install')
+  self.skipWaiting()
 
   event.waitUntil(
     caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Caching pages during install");
-      return cache.addAll(precacheFiles);
+      console.log('[PWA Builder] Caching pages during install')
+      return cache.addAll(precacheFiles)
     })
-  );
-});
+  )
+})
 
 // Allow sw to control of current page
-self.addEventListener("activate", function (event) {
-  console.log("[PWA Builder] Claiming clients for current page");
-  event.waitUntil(self.clients.claim());
-});
+self.addEventListener('activate', function (event) {
+  console.log('[PWA Builder] Claiming clients for current page')
+  event.waitUntil(self.clients.claim())
+})
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) { 
-  if (event.request.method !== "GET") return;
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return
 
   event.respondWith(
     fromCache(event.request).then(
@@ -38,28 +36,30 @@ self.addEventListener("fetch", function (event) {
         // file to use the next time we show view
         event.waitUntil(
           fetch(event.request).then(function (response) {
-            return updateCache(event.request, response);
+            return updateCache(event.request, response)
           })
-        );
+        )
 
-        return response;
+        return response
       },
       function () {
         // The response was not found in the cache so we look for it on the server
         return fetch(event.request)
           .then(function (response) {
             // If request was success, add or update it in the cache
-            event.waitUntil(updateCache(event.request, response.clone()));
+            event.waitUntil(updateCache(event.request, response.clone()))
 
-            return response;
+            return response
           })
           .catch(function (error) {
-            console.log("[PWA Builder] Network request failed and no cache." + error);
-          });
+            console.log(
+              '[PWA Builder] Network request failed and no cache.' + error
+            )
+          })
       }
     )
-  );
-});
+  )
+})
 
 function fromCache(request) {
   // Check to see if you have it in the cache
@@ -68,16 +68,16 @@ function fromCache(request) {
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
+        return Promise.reject('no-match')
       }
 
-      return matching;
-    });
-  });
+      return matching
+    })
+  })
 }
 
 function updateCache(request, response) {
   return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
+    return cache.put(request, response)
+  })
 }
